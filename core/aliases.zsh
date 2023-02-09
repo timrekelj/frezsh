8615c3ff2b98802adae2b9d80c7e1f6b9e69bec2
Tim Rekelj <62036887+timrekelj@users.noreply.github.com>
2023-02-09 09:57:29 +0100

    Update aliases.zsh

--- core/aliases.zsh
+++ core/aliases.zsh
@@ -1,13 +1,6 @@
 # Reload ZSH
 alias reload='source ~/.zsh/.zshrc'
 
-# exa
-alias ll='exa -lah'
-alias ls='exa'
-
-# bat
-alias cat='batcat'
-
 # Git
 alias gi='git init'
 alias ga='git add'
@@ -21,10 +14,3 @@ alias dc='docker-compose'
 alias dck='docker-compose kill'
 alias dcu='docker-compose up'
 alias d='docker'
-
-# Apt
-alias apt='nala'
-alias naur='sudo nala autoremove'
-alias nupdate='sudo nala update && sudo nala upgrade'
-alias ninst='sudo nala install'
-alias nurge='sudo nala purge'
